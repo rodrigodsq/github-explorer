ce9d47c57b0b4a977f14e1175e9a11d4f333b997
Rodrigo <rodrigodsq1@gmail.com>
2022-04-19 15:48:24 -0300

    add homepage package.json and router default

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
+    <Route path="/github-explorer" component={Dashboard} />
     <Route path="/repositories/:repository+" component={Repository} />
+    <Route render={() => <Redirect to="/github-explorer" />} />
   </Switch>
 );
 
